b0969414ab1c8c25cd02c2dcfd9a6f81caf0b5d7
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2023-03-12 09:26:40 -0300

    Criando os primeiros componentes reutilizáveis

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Box, createTheme } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
+import * as React from 'react';
+import { Header } from './components/Header';
+import { Layout } from './components/Layout';
+
+const theme = createTheme({});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+  return <ThemeProvider theme={theme}>
+    <Box component="main"
+      sx={{
+        height: '100vh',
+        // backgroundColor: '#000'
+      }}>
+
+    <Header />
+    <Layout>
+      <h1>Olá layout</h1>
+    </Layout>
+
+    </Box>
+  </ThemeProvider>
 }
 
 export default App;
